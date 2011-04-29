9ca8e9a67cf767babdd3a085767a677420ecb316
Héctor Menéndez <hector@gik.mx>
2011-04-29 00:55:06 -0500

    python/web-twitter-lists: Changed DB scheme yet another time

--- python/web-twitter-lists/db.sql
+++ python/web-twitter-lists/db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "list" ("id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, "id_list" TEXT , "id_user" TEXT , "name" TEXT);
-CREATE TABLE "member" ("id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, "id_member" TEXT , "id_list" TEXT , "name" TEXT);
+CREATE TABLE "lista" ("id" INTEGER PRIMARY KEY UNIQUE, "id_user" INTEGER , "name" TEXT);
+CREATE TABLE "member" ("id" INTEGER PRIMARY KEY UNIQUE, "id_list" INTEGER , "name" TEXT);
 CREATE TABLE "session" ("id" INTEGER PRIMARY KEY  NOT NULL  DEFAULT (0) ,"key" TEXT,"pwd" TEXT);
-CREATE TABLE "user" ("id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, "id_user" TEXT, "name" TEXT , "key" TEXT , "pwd" TEXT);
+CREATE TABLE "user" ("id" INTEGER PRIMARY KEY UNIQUE, "name" TEXT , "key" TEXT , "pwd" TEXT);
