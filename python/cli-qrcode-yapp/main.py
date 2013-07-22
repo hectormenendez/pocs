7cd78c761255fbbc5b154e6f3ad5e0f028b06542
Héctor Menéndez <hector@gik.mx>
2013-07-22 00:07:16 -0500

    python/cli-qrcode-yapp: last changes.

--- python/cli-qrcode-yapp/main.py
+++ python/cli-qrcode-yapp/main.py
@@ -1,16 +1,17 @@
 import QRCode
 
-cod = [{ "id":1069, "source":12, "target":1 },
-{ "id":1070, "source":12, "target":1 },
-{ "id":1071, "source":12, "target":1 },
-{ "id":1072, "source":12, "target":1 },
-{ "id":1047, "source":12, "target":1 },
-{ "id":1058, "source":12, "target":1 },
-{ "id":1030, "source":12, "target":1 },
-{ "id":1040, "source":12, "target":1 },
-{ "id":1094, "source":12, "target":1 },
-{ "id":1095, "source":12, "target":1 },
+cod = [
+    { "id":1530, "source":18, "target":1 },
+    { "id":1531, "source":18, "target":1 },
+    { "id":1532, "source":18, "target":1 },
+    { "id":1533, "source":18, "target":1 },
+    { "id":1534, "source":18, "target":1 },
+    { "id":1535, "source":18, "target":1 },
+    { "id":1536, "source":18, "target":1 },
+    { "id":1537, "source":18, "target":1 },
+    { "id":1538, "source":18, "target":1 },
 ]
+
 
 for x in cod:
 	qr = QRCode.Generate(x["id"], x["source"], x["target"])
