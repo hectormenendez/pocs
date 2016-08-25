b19227cc9e0f8303fa3b330d32d8b97baab9f08a
Héctor Menéndez <hector@gik.mx>
2016-08-25 02:53:27 -0500

    js-chrome/share-tab: Content now obtains images and keyword from current tab.

--- js-chrome/share-tab/lib/content.js
+++ js-chrome/share-tab/lib/content.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, reply) => {
-    const images = Array.prototype.slice
-        .call(document.getElementsByTagName('img'))
+    const domImages = document.getElementsByTagName('img');
+    const images = Array.prototype.slice.call(domImages)
         .map(({width,height,src}) => ({width, height, src}))
-    const kwords = document.querySelector('meta[name=keywords]').content;
-    reply({images, kwords});
+    const kwords = document.querySelector('meta[name=keywords]');
+    reply({images, kwords: kwords? kwords.content : null});
     return true;
-});
+});;
