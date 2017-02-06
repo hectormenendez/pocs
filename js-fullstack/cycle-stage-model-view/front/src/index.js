9ace328d0b51c11aee7af5a16cab074002095fc2
Héctor Menéndez <hector@gik.mx>
2017-02-05 21:11:25 -0600

    js-fullstack/cycle-stage-model-view: front: First step in real app, added a router.

--- js-fullstack/cycle-stage-model-view/front/src/index.js
+++ js-fullstack/cycle-stage-model-view/front/src/index.js
@@ -1,17 +1,36 @@
-import {makeDOMDriver as DomDriver} from '@cycle/dom';
-import {run as Run} from '@cycle/xstream-run';
+import {run              as Run}          from '@cycle/xstream-run';
+import {makeDOMDriver    as DomDriver}    from '@cycle/dom';
+import {makeRouterDriver as RouterDriver} from 'cyclic-router';
+import {createHistory    as History}      from 'history';
 
-import App from './root';
+import $          from 'xstream';
+import SwitchPath from 'switch-path';
+import SocketIO   from 'socket.io-client';
 
-// tempo
-import {makeHTTPDriver as HttpDriver} from '@cycle/http';
-import SocketIO from 'socket.io-client';
 import FeatherDriver from './drivers/feathers';
+import Routes        from './routes'
 
 const socket = SocketIO('http://localhost:3030', { path:'/ws/' });
 
-Run(App, {
+function main(sources){
+
+    const routes$ = sources.Router.define(Routes);
+    const router$ = routes$
+        .map(({path, value}) => {
+            const src = Object.assign({}, sources, { router: sources.Router.path(path) });
+            return value(src);
+        })
+        .map(page => page.DOM)
+        .flatten();
+
+    return {
+        DOM: router$,
+        router: $.of('/')
+    }
+}
+
+Run(main, {
     DOM      : DomDriver(document.getElementsByTagName('main')[0]),
-    HTTP     : HttpDriver(),
-    Feathers : FeatherDriver(socket)
+    Feathers : FeatherDriver(socket),
+    Router   : RouterDriver(History(), SwitchPath)
 });
