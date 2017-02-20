80ae7d1ca7d4eab0120f7fde583a6266bf638e3d
Héctor Menéndez <hector@gik.mx>
2017-02-19 18:32:17 -0600

    js-fullstack/cycle-stage-model-view: front: Basic navigation bar (still not a component).

--- js-fullstack/cycle-stage-model-view/front/src/pages/home/view.jsx
+++ js-fullstack/cycle-stage-model-view/front/src/pages/home/view.jsx
@@ -1,4 +1,25 @@
 import {html as Html} from 'snabbdom-jsx';
+import Style from './view.css';
 
 export default state => <section>
+    <nav role="navigation" className={Style.navigation}>
+
+        {/* Title bar */}
+        <h1>Money</h1>
+
+        {/* Hamburguer Icon */}
+        <input type="checkbox"/>
+        <aside>
+            <span/>
+            <span/>
+            <span/>
+        </aside>
+
+        {/* The actual menu */}
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+        </ul>
+    </nav>
 </section>
