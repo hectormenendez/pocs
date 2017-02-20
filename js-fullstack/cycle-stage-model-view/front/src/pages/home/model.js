08a5961a185abfe2f3319cc9ef361ffba68ff4e7
Héctor Menéndez <hector@gik.mx>
2017-02-20 11:48:05 -0600

    js-fullstack/cycle-stage-model-view: front: Added placeholder for home page using an (unfinished) Naviogation component.

--- js-fullstack/cycle-stage-model-view/front/src/pages/home/model.js
+++ js-fullstack/cycle-stage-model-view/front/src/pages/home/model.js
@@ -1,9 +1,23 @@
 import $ from 'xstream';
 
-export default function Model({ intent }){
+export default function Model({ intent, component }){
 
-    const vnode$ = $.of(null);
-    const state$ = $.of(null);
+    const navigation = {};
+
+    navigation.component$ = component.navigation({ hola: 'mundo' });
+
+    navigation.state$ = navigation.component$
+        .map(component => component.state$)
+        .flatten()
+        .map(state => ({ navigation: state }));
+
+    navigation.vnode$ = navigation.component$
+        .map(component => component.DOM)
+        .flatten()
+        .map(vnode => ({ Navigation: () => vnode }));
+
+    const state$ = navigation.state$.startWith({});
+    const vnode$ = navigation.vnode$;
 
     return { state$, vnode$ }
 }
--- js-fullstack/cycle-stage-model-view/front/src/pages/home/stage.js
+++ js-fullstack/cycle-stage-model-view/front/src/pages/home/stage.js
@@ -1,4 +1,12 @@
+import $ from 'xstream';
+import {Component} from '../../helpers/smv';
+
 export default function Stage(source){
+
+    const component = {};
+    component.navigation = Component({ source, path:'components/navigation' });
+
     const intent = {};
-    return { intent };
+
+    return { intent, component };
 }
