99bd2540d89dd8b708bcb7b29d97493b7d7e3dcd
Héctor Menéndez <hector@gik.mx>
2017-03-06 04:29:24 -0600

    js-fullstack/cycle-stage-model-view: front: Appended root style to entry poiint.

--- js-fullstack/cycle-stage-model-view/front/src/index.js
+++ js-fullstack/cycle-stage-model-view/front/src/index.js
@@ -1,14 +1,15 @@
-import { run              as Run          } from '@cycle/xstream-run';
-import { makeDOMDriver    as DomDriver    } from '@cycle/dom';
+import { run as Run } from '@cycle/xstream-run';
+import { makeDOMDriver as DomDriver } from '@cycle/dom';
 import { makeRouterDriver as RouterDriver } from 'cyclic-router';
-import { createHistory    as History      } from 'history';
+import { createHistory as History } from 'history';
 
-import $          from 'xstream';
+import $ from 'xstream';
 import SwitchPath from 'switch-path';
-import SocketIO   from 'socket.io-client';
+import SocketIO from 'socket.io-client';
 
 import FeatherDriver from './drivers/feathers';
-import Routes$        from './routes'
+import Routes$ from './routes';
+import './index.css';
 
 const socket = SocketIO('http://localhost:3030', { path:'/ws/' });
 
