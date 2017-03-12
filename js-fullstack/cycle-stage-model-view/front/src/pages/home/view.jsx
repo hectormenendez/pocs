221b07ee83effeb23fa7b423f4ea02103f1c54b1
Héctor Menéndez <hector@gik.mx>
2017-03-12 13:19:58 -0600

    js-fullstack/cycle-stage-model-view: front: PoC: for @inherit rules using postcss-inherit propietary plugin.

--- js-fullstack/cycle-stage-model-view/front/src/pages/home/view.jsx
+++ js-fullstack/cycle-stage-model-view/front/src/pages/home/view.jsx
@@ -1,22 +1,11 @@
 import {html as Html} from 'snabbdom-jsx';
+
 import Style from './view.css';
 
 export default (state, {Navigation}) => <section>
     <Navigation/>
-    <section className={Style.test}>
-        <ul>
-            <li> My App </li>
-            <li> Create new </li>
-            <li attrs-data-name="home"> Home </li>
-            <li> About </li>
-            <li> News </li>
-            <li> Help </li>
-        </ul>
-    </section>
-    <section className={Style.table}>
-
+    <section className={Style['ui-table']}>
         <table>
-            <caption>This is a caption</caption>
             <thead>
                 <tr>
                     <th>Username</th>
@@ -26,14 +15,19 @@ export default (state, {Navigation}) => <section>
             </thead>
             <tbody>
                 <tr>
-                    <td>etor</td>
-                    <td>Héctor</td>
-                    <td>Menéndez</td>
+                    <td>Jim</td>
+                    <td>Kirk</td>
+                    <td>@captaink</td>
                 </tr>
                 <tr>
-                    <td>mariela</td>
-                    <td>Mariela</td>
-                    <td><a href="#">Hola</a></td>
+                    <td>Mr.</td>
+                    <td>Spock</td>
+                    <td>@sciencie101</td>
+                </tr>
+                <tr>
+                    <td>Nyota</td>
+                    <td>Uhura</td>
+                    <td>@comms</td>
                 </tr>
             </tbody>
         </table>
