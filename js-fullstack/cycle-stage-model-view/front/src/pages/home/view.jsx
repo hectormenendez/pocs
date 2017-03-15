9a80863bcd85f5974f090526061721be73d445f1
Héctor Menéndez <hector@gik.mx>
2017-03-14 18:54:19 -0600

    js-fullstack/cycle-stage-model-view: front: Defined frontend for home using Navigation and Buttons.

--- js-fullstack/cycle-stage-model-view/front/src/pages/home/view.jsx
+++ js-fullstack/cycle-stage-model-view/front/src/pages/home/view.jsx
@@ -2,10 +2,11 @@ import {html as Html} from 'snabbdom-jsx';
 
 import Style from './view.css';
 
-export default (state, {Navigation}) => <section>
-    <Navigation/>
-    <ul>
-        <li><a href="#/examples/todo">todo</a></li>
-        <li><a href="http://google.com">google</a></li>
-    </ul>
-</section>
+export default (state, {Navigation}) =>
+    <section>
+        <Navigation/>
+        <section className={Style.home}>
+            <a href="#/cuentas" className={Style.button}>Cuentas</a>
+            <a href="#/movs" className={Style.button}>Movs</a>
+        </section>
+    </section>
