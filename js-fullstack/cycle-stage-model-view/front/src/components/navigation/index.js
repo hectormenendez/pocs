e9360fbb330c2ff414b9a8ccfec4f16bfafb9bb0
Héctor Menéndez <hector@gik.mx>
2017-03-23 02:25:30 -0600

    js-fullstack/cycle-stage-model-view: front: components/navigation: Removed component logic from index.js, it is now handled by SMV's component.

--- js-fullstack/cycle-stage-model-view/front/src/components/navigation/index.js
+++ js-fullstack/cycle-stage-model-view/front/src/components/navigation/index.js
@@ -1,26 +1,3 @@
 import {Component} from '../../helpers/smv';
 
-// Wrapper for easy usage of the Navigation plugin.
-export default function Navigation(source){
-    const component = Component({ source, path: 'components/navigation' });
-
-    return (...args) => {
-        const component$ = component(...args);
-        return {
-            // The state will always be inside a navigation property
-            State: component$
-                .map(component => component.State)
-                .flatten()
-                .map(state => ({ navigation: state })),
-            // Expose the VirtualTree as a function so it can be used as a Tag
-            DOM: component$
-                .map(component => component.DOM)
-                .flatten()
-                .map(dom => ({ Navigation: () => dom })),
-            // Expose the Router stream as is.
-            Router: component$
-                .map(component => component.Router)
-                .flatten()
-        }
-    }
-}
+export default sources => Component({ sources, path: 'components/navigation' });
