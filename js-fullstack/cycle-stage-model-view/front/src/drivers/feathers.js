5b381f05d63bf0368b99b97cc33eee703791c049
Héctor Menéndez <hector@gik.mx>
2017-03-27 05:22:19 -0600

    js-fullstack/cycle-stage-model-view: front: drivers/feather: Experimenting with a new version of the driver. Needs review.

--- js-fullstack/cycle-stage-model-view/front/src/drivers/feathers.js
+++ js-fullstack/cycle-stage-model-view/front/src/drivers/feathers.js
@@ -1,6 +1,6 @@
-import $        from 'xstream';
+import $ from 'xstream';
 import Feathers from 'feathers-client';
-import Emitter  from 'event-emitter';
+import Debug from 'debug';
 
 import Validate from '../helpers/validate';
 
@@ -8,66 +8,69 @@ export default function (socket){
 
     if (!socket) throw new Error('Expecting a socket connection');
 
-    const event = new Emitter();
+    const debug = Debug('feathers:driver');
+
+    const client = Feathers()
+        .configure(Feathers.hooks())
+        .configure(Feathers.socketio(socket))
 
     // Returns a function that accepts a Sink Stream and returns a source.
     return function (sink$) {
-
-        // The feathers application
-        const app = Feathers()
-            .configure(Feathers.hooks())
-            .configure(Feathers.socketio(socket));
 
         // Process the received sink stream.
         // - emits the specified method on the socket service
         // - triggers a local event that can be later selected from the sources.
         sink$
             .map(sink => {
-                Validate(sink, { service:[String], method:[String], args:Array });
-                const service = app.service(sink.service);
-                const method  = service[sink.method];
-                const args    = sink.args || [];
+                Validate(sink, { service: [String], method: [String], args: Array });
+                const event = `${sink.service}::${sink.method}`;
+                const service = client.service(sink.service);
+                const method = service[sink.method];
+                const args = sink.args || [];
                 if (typeof method !== 'function')
                     throw new Error(`Invalid method '${sink.method}' for ${sink.service}`);
+                debug(`sink request → ${event}`, args);
                 return $
                     .fromPromise(service[sink.method](...args))
-                    .map(response => ({ ...sink, response }));
+                    .map(response => ({ ...sink, response, event }))
             })
             .flatten()
             .addListener({
-                // just throw the errors if any.
-                error: err => { throw err },
-                // emit the corresponding event
-                next: ({service, method, response}) => event
-                    .emit(`${service}:${method}`, response),
-            });
+                error: error => { throw error },
+                next : sink => {
+                    // emit locally
+                    debug(`sink response → ${sink.event}`, sink.response);
+                    client.emit(sink.event, sink.response);
+                }
+            })
 
         // The source stream that will handle the intents
         return {
-            // make this 'hackable', expose the application.
-            app,
             // Handles selection (for local events)
-            select: selector => {
-                Validate(selector, { service:[String], method:[String] });
-                const { service, method } = selector;
-                function start(listener){
-                    return event.on(`${service}:${method}`, function(response){
-                        return listener.next(response);
-                    })
-                }
-                return $.create({ start , stop:()=>{} }); // stop won't happen
-            },
-            // Handles sources selection. (for remote events)
-            on: event => {
-                Validate(event, { service:[String], method:[String] });
-                const service = app.service(event.service);
-                function start(listener){
-                    return service.on(event.method, function(response){
-                        return listener.next(response);
-                    })
-                }
-                return $.create({ start, stop: ()=>{} }) // stop won't happen
+            on: selector => {
+                const { type, service, method } = Validate(selector, {
+                    service: [String],
+                    method: [String],
+                    type: [String]
+                });
+                const event = `${service}::${method}`;
+                return $.create({ stop(){}, start(listener){
+                    const handler = response => {
+                        debug(`source ${type}:emitted → ${service}::${method}`, response);
+                        listener.next(response);
+                    };
+                    if (type == 'local') client.on(event, handler);
+                    if (type == 'socket') client.service(service).on(method, handler);
+                    debug(`source ${type}:added → ${event}`)
+                }});
             },
         };
     };
+}
+
+function typeValidate(type, event){
+    const types = ['local', 'socket'];
+    const msg = `Invalid type for ${event}, expecting [${types.join(',')}]; got: ${type}`;
+    if (types.indexOf(type) === -1) throw new Error(msg);
+    return true;
 }
