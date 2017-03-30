16bcda96b264bf0a877b283c35de6eb77dae105f
Héctor Menéndez <hector@gik.mx>
2017-03-30 08:52:24 -0600

    js-fullstack/cycle-stage-model-view: front: component/examples: Deleted, there's a branch for this now.!

--- js-fullstack/cycle-stage-model-view/front/src/components/examples/slider/view.jsx
+++
@@ -1,6 +0,0 @@
-import {html as Html} from 'snabbdom-jsx';
-
-export default state => <section>
-    <label>{state.type} {state.value} {state.unit} </label>
-    <input type="range" min={state.min} max={state.max} value={state.value} />
-</section>
