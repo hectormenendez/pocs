98fb0761eb5211bd7a66241e13c1b7314ada4e32
Héctor Menéndez <hector@gik.mx>
2017-06-25 22:44:28 -0500

    js-browser/canvas-snake-old: Refactored to a grid-system

--- js-browser/canvas-snake-old/canvas.js
+++ js-browser/canvas-snake-old/canvas.js
@@ -4,11 +4,13 @@ class Canvas {
         this.settings = Object.assign({}, Canvas.settings, settings);
         this.element = document.querySelector('canvas');
         this.context = this.element.getContext('2d');
+        this.grid = { x:[], y:[], size:this.settings.size };
         this.setup();
     }
 
     static get settings(){
         return Object.freeze({
+            color: '#444',
             size: 10
         });
     }
@@ -20,13 +22,27 @@ class Canvas {
         this.orientation = (w > h? 'landscape' : (w < h? 'portrait' : 'square'));
         this.ratio = w / h;
         // Create a virtual grid on canvas
-        this.grid = { x:[], y:[] };
-        for (let i=0; i < Math.floor(w / this.settings.size); i++)
-            this.grid.x.push(i*this.settings.size);
-        for (let i=0; i < Math.floor(h / this.settings.size); i++)
-            this.grid.y.push(i*this.settings.size);
+        for (let i=0; i < Math.floor(w / this.grid.size); i++)
+            this.grid.x.push(i * this.grid.size);
+        for (let i=0; i < Math.floor(h / this.grid.size); i++)
+            this.grid.y.push(i * this.grid.size);
         // determine canvas size from the grid
-        this.element.width = this.settings.size * this.grid.x.length;
-        this.element.height = this.settings.size * this.grid.y.length;
+        this.element.width = this.grid.size * this.grid.x.length;
+        this.element.height = this.grid.size * this.grid.y.length;
     }
+
+    update(){
+        this.context.fillStyle = this.settings.color;
+        this.context.fillRect(0, 0, this.element.width, this.element.height);
+    }
+
+    getPosition({x, y}){
+        if (x && !y) return this.grid.x[x];
+        if (y && !x) return this.grid.y[y];
+        return {
+            x: this.grid.x[x],
+            y: this.grid.y[y]
+        };
+    }
+
 }
--- js-browser/canvas-snake-old/target.js
+++ js-browser/canvas-snake-old/target.js
@@ -1,5 +1,26 @@
 class Target {
-    constructor(){
-        console.log('target')
+
+    constructor(settings){
+        this.settings = Object.assign({}, Target.settings, settings);
+        this.position = null;
+    }
+
+    static get settings(){
+        return Object.freeze({
+            color: 'tomato'
+        });
+    }
+
+    setup(canvas){
+        this.position = {
+            x: Math.round(Math.random() * canvas.grid.x.length),
+            y: Math.round(Math.random() * canvas.grid.y.length)
+        };
+    }
+
+    update(canvas, move=false){
+        const {x,y} = canvas.getPosition(this.position);
+        canvas.context.fillStyle = this.settings.color;
+        canvas.context.fillRect(x, y, canvas.grid.size, canvas.grid.size);
     }
 }
