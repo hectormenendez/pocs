a606d9766f551a9c161ad6c071d5d7fae45a2f6a
Héctor Menéndez <hector@gik.mx>
2017-09-09 15:52:34 -0500

    js-fullstack/cycle-music: @front/setup: Added webpack, and webpack-dev-server as dependencies.

---
+++ js-fullstack/cycle-music/scripts/start.js
@@ -0,0 +1,9 @@
+// NPM modules
+import Webpack from 'webpack';
+import Server from 'webpack-dev-server';
+// Local
+import Config from 'config/webpack.server';
+
+
+console.log(Config);
+process.exit(0);
