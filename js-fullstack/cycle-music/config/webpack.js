3c4ed61bcb30e76db50b28f77ba161a2563756c3
Héctor Menéndez <hector@gik.mx>
2017-09-09 20:34:44 -0500

    js-fullstack/cycle-music: @front/setup: Defined the common webpack config.

--- js-fullstack/cycle-music/config/webpack.js
+++ js-fullstack/cycle-music/config/webpack.js
@@ -1,5 +1,72 @@
+// Node modules
+import PATH from 'path';
+// NPM modules
+import Webpack from 'webpack';
+import WebpackHtml from 'html-webpack-plugin';
+// Local modules
 import Path from 'util/path';
+import ConfigApp from 'config/app';
 
 export default {
-    common:true
+
+    // The target environment for the compilation
+    target: 'web',
+
+    // The directory containing the entry files
+    context: Path.source_front,
+
+    // The application starting point
+    entry: 'index.js',
+
+    // Instructions on how to output the bundle
+    output: {
+        // Use JSONP for loading on-demand chunks, by adding a script tag.
+        crossOriginLoading: false,
+        // Where should the bundle be put?
+        path: Path.output,
+        // The public URL for the root on the browser
+        publicPath: ""
+    },
+
+    // Transformations applied to each file-type imported on the app
+    module: [
+        // Html files.
+        // These are the index files that will be put at the root of the output folder.
+        {
+            test: /\.html$/,
+            loader: 'html-loader',
+            include: PATH.join(Path.source_front, 'index.html')
+        },
+        {
+            // js or jsx files
+            // Apply the babel-loader so they get properly transpiled
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            include: Path.source_front,
+            query: {
+                // Cache the directory after the first run, so the loader can save time.
+                cacheDirectory: true
+            }
+        }
+    ],
+
+    plugins: [
+        // Outputs an html file based upon a template (specified on the html-loader)
+        new WebpackHtml({
+            // The title to use for the generated html
+            title: ConfigApp.title,
+            // The filename to use for the generated html
+            filename: 'index.html',
+            // Use this file as a template for the generated html
+            template: PATH.join(Path.source_front, 'index.html'),
+            // Inject generated resources on the head element of the html
+            inject: 'head',
+            // Append hashes to every generated resources to avoid caching them
+            hash: process.env.NODE_ENV !== 'production',
+            // Only emit the file when it hasn't changed between compiles
+            cache: process.env.NODE_ENV !== 'production',
+            // Write error details on the webpage
+            showErrors: true
+        })
+    ]
 }
