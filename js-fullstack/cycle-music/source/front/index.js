3eb599c3f732049f3e9e31431a8f8140fc37b75c
Héctor Menéndez <hector@gik.mx>
2017-09-10 00:39:22 -0500

    js-fullstack/cycle-music: @front/setup: Enabled cycle-restart, and verified normal dependencies

--- js-fullstack/cycle-music/source/front/index.js
+++ js-fullstack/cycle-music/source/front/index.js
@@ -1,1 +1,15 @@
-console.log('Hola Mundo');
+// NPM modules
+import $ from 'xstream';
+import Isolate from '@cycle/isolate';
+import Onionify from 'cycle-onionify';
+import { setup as Setup, run as Run } from '@cycle/run';
+import { makeDOMDriver as DriverDOM } from '@cycle/dom';
+import { makeHTTPDriver as DriverHTTP } from '@cycle/http';
+import { timeDriver as DriverTime } from '@cycle/time';
+import { restartable as Restartable, rerunner as Rerunner } from 'cycle-restart';
+
+/// #if PRODUCTION
+console.log('here');
+/// #else
+console.log('there');
+/// #endif
