63f7a7351d7676f53a80501a1585763fee2780ba
Héctor Menéndez <hector@gik.mx>
2017-09-24 05:02:45 -0500

    js-fullstack/cycle-music: @tools/scripts: Added backend manager script. (base)

---
+++ js-fullstack/cycle-music/source/tools/scripts/start.back.js
@@ -0,0 +1,35 @@
+// Node modules
+import PATH from 'path';
+// NPM modules
+import Nodemon from 'nodemon';
+
+// Local modules
+import Path from 'tools/path';
+
+const commands = [];
+// Parses the backend source using eslint
+commands.push([
+    PATH.join(Path.modules, '.bin', 'eslint'),
+    PATH.join(Path.back, '**', '*.js'),
+].join(' '));
+
+// Runs the backend source trough babel-node
+commands.push([
+    `NODE_PATH=${Path.back}:${Path.source}`,
+    PATH.join(Path.modules, '.bin', 'babel-node'),
+    Path.back,
+].join(' '));
+
+Nodemon({
+    restartable: 'rs',
+    ext: 'js json',
+    verbose: true,
+    watch: Path.back,
+    exec: commands.join('&&'),
+
+})
+    .on('restart', () => process.stdout.write('\n\n RESTART \n\n'))
+    .on('quit', function(){
+        console.log('quit');
+        process.exit(0);
+    });
