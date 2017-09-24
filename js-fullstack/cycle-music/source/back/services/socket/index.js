7d1b169a0bfaa61fb3f04b33913873709b980317
Héctor Menéndez <hector@gik.mx>
2017-09-24 17:18:35 -0500

    js-fullstack/cycle-music: @back/services: Added hooks for socket service.

--- js-fullstack/cycle-music/source/back/services/socket/index.js
+++ js-fullstack/cycle-music/source/back/services/socket/index.js
@@ -3,6 +3,7 @@
 import ServiceNeDB from 'feathers-nedb';
 // Local modules
 import ModelNeDB from 'models/nedb';
+import Hooks from './hooks';
 
 export default function ServiceSocket() {
     const name = 'socket';
@@ -10,4 +11,5 @@ export default function ServiceSocket() {
     this.use(`/${name}`, ServiceNeDB({ name, Model: ModelNeDB(`${name}.db`) }));
     // Initiaize the service
     const service = this.service(name);
+    service.hooks(Hooks);
 }
