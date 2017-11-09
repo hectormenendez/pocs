f9337132e0b42127079561a7b9c36f193b5aec2b
Hector Menendez <hector@gik.mx>
2017-11-09 14:57:32 -0600

    js-fullstack/gql-example-relay: Enabled GraphiQL GUI

--- js-fullstack/gql-example-relay/src/server.js
+++ js-fullstack/gql-example-relay/src/server.js
@@ -1,8 +1,10 @@
 // Modules
 import Express from 'express';
+import ExpressGraphQL from 'express-graphql'
 // Local
 import Config from 'config';
 import Log from 'logger';
+import Schema from 'schema';
 
 const Server = Express();
 export default Server;
@@ -11,6 +13,14 @@ Server.get('/', (request, response) => {
     response.send('GraphQL & Relay modern is cools!!!');
     return true;
 });
+
+const root = { hello: () => "Hi, I'm Hector" };
+
+Server.use('/graphql', ExpressGraphQL({
+    graphiql: true, // enable GUI
+    schema: Schema,
+    rootValue: root,
+}));
 
 Server.listen(Config.port, Config.host, () =>
     Log.info('Running server on %s:%s', Config.host, Config.port),
