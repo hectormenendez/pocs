41696a3f0210dbdc650dc9b62074a97b246c11a9
Héctor Menéndez <hector@gik.mx>
2017-11-11 14:40:27 -0600

    js-fullstack/gql-example-apollo: both: Created symlink for schema to only change once.

--- js-fullstack/gql-example-apollo/back/src/schema.js
+++ js-fullstack/gql-example-apollo/back/src/schema.js
@@ -3,9 +3,13 @@ import {
     addMockFunctionsToSchema as MockSchema,
 } from 'graphql-tools';
 // Local
+import { Resolvers } from 'resolvers';
 import Schema from './schema.graphql';
 
-const schema = MakeSchema({ typeDefs: Schema });
+const schema = MakeSchema({
+    typeDefs: Schema,
+    resolvers: Resolvers,
+});
 
 MockSchema({ schema });
 
