9463dc4528a01716eda5983560010013336161f0
Héctor Menéndez <hector@gik.mx>
2017-11-12 11:32:19 -0600

    js-fullstack/gql-example-apollo: back: Added subcription handlers for notes.

--- js-fullstack/gql-example-apollo/back/src/resolvers.js
+++ js-fullstack/gql-example-apollo/back/src/resolvers.js
@@ -1,26 +1,44 @@
 import CRYPTO from 'crypto';
+import { PubSub, withFilter as WithFilter } from 'graphql-subscriptions';
+
+const pubsub = new PubSub();
 
 export const Data = [
     {
         id: 1,
         firstName: 'Maria Eugenia',
         lastName: 'Rivera Rodriguez',
+        notes: [
+            {
+                id: '1',
+                content: 'Mi mami chula.',
+            },
+            {
+                id: '2',
+                content: 'Es una enojona.',
+            },
+        ],
     },
     {
         id: 2,
         firstName: 'Mariela Eugenia',
         lastName: 'Vallejo Rivera',
+        notes: [
+        ],
     },
     {
         id: 3,
         firstName: 'Mario Luis',
         lastName: 'Vallejo Verón',
+        notes: [
+        ],
     },
 ];
 
 export const Resolvers = {
     Query: {
         contacts: () => Data,
+        contact: (root, { id }) => Data.find(contact => contact.id === id),
     },
     Mutation: {
         addContact: (root, { firstName, lastName }) => {
@@ -31,6 +49,24 @@ export const Resolvers = {
             };
             Data.push(contact);
             return contact;
+        },
+        addNote: (root, { note: { contactId, content } }) => {
+            const contact = Data.find(item => item.id === contactId);
+            const note = {
+                id: CRYPTO.randomBytes(5).toString('hex'),
+                content,
+            };
+            contact.push(note);
+            pubsub.publish('noteAdded', { note, contactId });
+            return note;
+        },
+    },
+    Subscription: {
+        noteAdded: {
+            subscribe: WithFilter(
+                () => pubsub.asyncIterator('noteAdded'),
+                ({ contactId }, variables) => variables.contactId === contactId,
+            ),
         },
     },
 };
