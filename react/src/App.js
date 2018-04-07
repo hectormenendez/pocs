a55845ecb8a1f155a5c928e72392282b4b030097
Hector Menendez <hector@gik.mx>
2018-04-06 22:29:21 -0500

    Simple implementation antd on template.

--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Button from 'antd/lib/button';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class extends React.Component {
+    render() {
+        return <div className="App">
+            <Button type="primary">Button</Button>
+        </div>;
+    }
 }
-
-export default App;
