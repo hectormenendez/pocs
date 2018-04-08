b49bb761a24035144e1645406c1feaaf92f95052
Hector Menendez <hector@gik.mx>
2018-04-08 12:59:35 -0500

    Added a basic home layout as a test

---
+++ react/src/layouts/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Layout, Breadcrumb } from 'antd';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { Content as StyleContent } from '~/layouts/index.module.css';
+import { Breadcrumb as StyleBreadcrumb } from './index.module.css';
+
+export default () => <Layout.Content className={StyleContent}>
+    <Breadcrumb className={StyleBreadcrumb}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Tempo</Breadcrumb.Item>
+    </Breadcrumb>
+    <section>
+        <RouterLink to="/accounts">Accounts</RouterLink>
+    </section>
+</Layout.Content>;
