0c385eda96504a7b1d5c645937d8024fd5d01e98
Héctor Menéndez <hector@gik.mx>
2018-04-12 04:55:55 -0500

    PoC for fetch api, downloaded Todoist items.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import Config from './config.json';
 
 const styles = StyleSheet.create({
     container: {
@@ -8,13 +10,46 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    text: {
+        fontSize: 40,
+    },
 });
 
 export default class App extends React.Component {
+
+    state = {
+        token: Config.token,
+        sync_token: '*',
+        resource_types: JSON.stringify(['items']),
+    };
+
+    componentDidMount() {
+
+        // A proper x-www-form-urlencoded paylod has to be created.
+        const body = [
+            `token=${this.state.token}`,
+            `sync_token=${this.state.sync_token}`,
+            `resource_types=${encodeURIComponent(this.state.resource_types)}`,
+        ].join('&');
+
+        fetch('https://todoist.com/api/v7/sync', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body,
+        })
+        .then(response => response.json())
+        .then(json => console.log(json));
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello World!</Text>
+                <Button
+                    title="Count"
+                    onPress={() => this.setState({ count: this.state.count + 1 })} />
+                <Text style={styles.text}>{this.state.count}</Text>
             </View>
         );
     }
