9ebc279be64ecfc2414064e2bf67c6a5a1f89742
Héctor Menéndez <hector@gik.mx>
2018-04-24 15:39:11 -0500

    Simple implementation of todoist api via `todoist-js`

--- back/src/index.js
+++ back/src/index.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+import Todoist from 'todoist-js';
+import { todoist as Config } from '../config.json';
+
+const todoist = new Todoist(Config.token);
+
+todoist.completed.get_stats().then(stats => console.log(stats));
