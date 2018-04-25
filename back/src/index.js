4989e47cf8f56a15f539cad89c612e62845df527
Héctor Menéndez <hector@gik.mx>
2018-04-25 02:35:00 -0500

    Created  "menu" to select an action.

--- back/src/index.js
+++ back/src/index.js
@@ -1,7 +1,22 @@
+import { $fromInput } from './utils';
 import GomodoroCounter$ from './gomodoro-counter';
 
+const prompt = [
+    'Select an action:',
+    '   [1] Gomodoro counter.',
+    '',
+].join('\n');
+
+const input$ = $fromInput(prompt)
+    .switchMap((input) => {
+        switch (input) {
+            case '1': return GomodoroCounter$;
+            default: throw new Error('Invalid option.');
+        }
+    });
+
 /* eslint-disable no-console */
-GomodoroCounter$.subscribe(
+input$.subscribe(
     response => console.log('Response:', response),
     (error) => {
         console.error('Error:', error);
