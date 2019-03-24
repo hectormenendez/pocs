54abe8652a9cc7ae657474b6c98180f9d31dbdf1
Héctor Menéndez <hector@gik.mx>
2019-03-24 00:07:03 -0500

    automation/instagram: bot: Implemented media getting.

--- automation/instagram/src/browser.py
+++ automation/instagram/src/browser.py
@@ -1,6 +1,7 @@
 # python libs
 from time import sleep as Sleep
 from functools import reduce as Reduce
+from inspect import currentframe as CurrentFrame, getargvalues as ArgValues
 # virtualenv libs
 from json import loads as Parse, dumps as Stringify
 from urllib.parse import quote_plus as Quote
@@ -8,12 +9,14 @@ from selenium import webdriver as Driver
 # local libs
 from common import CONF, PATH
 
+LIMIT = 9999
 URL_BASE = "https://www.instagram.com"
 URL_QUERY = URL_BASE + "/graphql/query/?query_hash={}&variables={}"
 
 ERROR = "ERROR: {}"
 ERROR_NOT_IMPLEMENTED = ERROR.format("Not implemented yet")
 ERROR_EXPECTING = ERROR.format("Expecting {}")
+
 
 class Bot():
 
@@ -24,33 +27,38 @@ class Bot():
         self.driver.implicitly_wait(30)
         self.throttle = throttle
 
-    def __get_json(self, url):
+    def _get_json(self, url):
         Sleep(self.throttle)
         self.driver.get(url)
-        text = self.driver.find_element_by_tag_name("pre").get_attribute("innerHTML")
-        return Parse(text)
+        try:
+            text = self.driver.find_element_by_tag_name("pre").get_attribute("innerHTML")
+            return Parse(text)
+        except Exception as error:
+            print(error)
+            self.halt()
 
-    def __get_json_query(self, id=False, vars={}, query=False, route=False, page=False, accumulator=[]):
+    def _get_json_query(self, id=False, vars={}, query=False, route=False, page=False, accumulator=[]):
         # is this an iterative call? append the variable to enable pagination.
         if page:
             vars["after"] = page
         # Get the raw json data from the corresponding URL
-        json = self.__get_json(URL_QUERY.format(query, Quote(Stringify(vars))))
+        json = self._get_json(URL_QUERY.format(query, Quote(Stringify(vars))))
         # according to the specified route, get the correct node.
-        json = Reduce(lambda data, key: data[key], route, json)
+        json = Reduce(lambda acc, cur: acc[cur], route, json)
+        # if there are no edges available, then it's a simple json, no need to further parsing.
+        if "edges" not in json:
+            return json
         # given the current accumulated value, grab raw items from edges,
         accumulator = accumulator + list(
             map(lambda edge: edge["node"], json["edges"])
         )
         # are there more pages available? be recursive and get them.
         if json["page_info"]["has_next_page"]:
-            return self.__get_json_query(
-                id=id,
-                query=query,
-                route=route,
-                page=json["page_info"]["end_cursor"],
-                accumulator=accumulator
-            )
+            # recursively send the same parameters available,
+            # except the last two, which will be updated.
+            arg_keys, _, _, arg_vals = ArgValues(CurrentFrame())
+            arg = list(map(lambda key: arg_vals[key], arg_keys[1:-2]))
+            return self._get_json_query(*(arg + [json["page_info"]["end_cursor"], accumulator]))
         # no more pages return the accumulated value.
         return accumulator
 
@@ -82,7 +90,7 @@ class Bot():
         self.driver.get("{}/{}/?__a=1".format(URL_BASE, username))
 
     def get_user(self, username=CONF["user"]["name"]):
-        json = self.__get_json("{}/{}/?__a=1".format(URL_BASE, username))
+        json = self._get_json("{}/{}/?__a=1".format(URL_BASE, username))
         user = json["graphql"]["user"]
         return {
             "id": user["id"],
@@ -99,8 +107,44 @@ class Bot():
             "verified": user["is_verified"],
         }
 
+    def get_media(self, shortcode=False):
+        media = self._get_json_query(
+            query="477b65a610463740ccdb83135b2014db",
+            route=["data", "shortcode_media"],
+            vars={
+                "shortcode": shortcode,
+                "child_comment_count": LIMIT,
+                "fetch_comment_count": LIMIT,
+                "parent_comment_count": LIMIT,
+                "has_threaded_comments": False,
+            },
+        )
+        # get a desription
+        caption = False
+        if "edges" in media["edge_media_to_caption"]:
+            caption = media["edge_media_to_caption"]["edges"][0]["node"]["text"]
+        likes = 0
+        if "edge_liked_by" in media:
+            likes = media["edge_liked_by"]["count"]
+        elif "edge_media_preview_like" in media:
+            likes = media["edge_media_preview_like"]["count"]
+        return {
+            "id": media["id"],
+            "shortcode": media["shortcode"],
+            "caption": caption,
+            "url": media["display_url"],
+            "location": media["location"] if "location" in media else False,
+            "comments": media["edge_media_to_comment"]["count"],
+            "likes": likes,
+            "is_video": media["is_video"],
+            "is_liked": media["viewer_has_liked"],
+            "is_saved": media["viewer_has_saved"],
+            "is_tag": media["viewer_in_photo_of_you"],
+            "is_ad": media["is_ad"],
+        }
+
     def get_user_following(self, id):
-        users = self.__get_json_query(
+        users = self._get_json_query(
             query="c56ee0ae1f89cdbd1c89e2bc6b8f3d18",
             route=["data", "user", "edge_follow"],
             vars={
@@ -117,7 +161,7 @@ class Bot():
         )
 
     def get_user_followers(self, id):
-        users = self.__get_json_query(
+        users = self._get_json_query(
             query="56066f031e6239f35a904ac20c9f37d9",
             route=["data", "user", "edge_followed_by"],
             vars={
@@ -134,7 +178,7 @@ class Bot():
         )
 
     def get_user_medias(self, id):
-        return self.__get_json_query(
+        medias = self._get_json_query(
             query="f2405b236d85e8296cf30347c9f08c2a",
             route=["data", "user", "edge_owner_to_timeline_media"],
             vars={
@@ -144,62 +188,17 @@ class Bot():
                 "first": 9999,
             },
         )
-
-    def get_media(self, shortcode=False):
-        media = self.__get_json_query(
-            query="477b65a610463740ccdb83135b2014db",
-            route=["data", "shortcode_media"],
-            vars={
-                "shortcode": shortcode,
-                "child_comment_count": 3,
-                "fetch_comment_count": 40,
-                "parent_comment_count": 24,
-                "has_threaded_comments": False,
-            },
-        )
-        print(media)
-        # media = media["node"]
-        # if not id and not media:
-        #     self.halt(ERRROR_EXPECTING.format("either an id or a media"))
-        # if id:
-        #     # TODO: Find a way to retrieve a media by id
-        #     self.halt(ERROR_NOT_IMPLEMENTED)
-        # if not media:
-        #     return {}
-        # text = False
-        # if len(media["edge_media_to_caption"]["edges"]):
-        #     text = media["edge_media_to_caption"]["edges"][0]["node"]["text"]
-        # return {
-        #     "id": media["id"],
-        #     "text": text,
-        #     "comments": media["edge_media_to_comment"]["count"],
-        #     "likesReal": media["edge_liked_by"]["count"],
-        #     "likesShow": media["edge_media_preview_like"]["count"],
-        #     "isVideo": media["is_video"],
-        #     "pic": media["thumbnail_src"],
-        #     # TODO: find out why this is not working for all medias
-        #     # "caption": media["accessibility_caption"],
-        # }
-
+        def mediator(media):
+            media = self.get_media(shortcode=media["shortcode"])
+            media["user"] = id
+            return media
+        return list(map(mediator, medias))
 
 
 bot = Bot()
 bot.login()
 user = bot.get_user()
 # user["followers"] = bot.get_user_followers(id=user["id"])
-medias = bot.get_user_medias(id=user["id"])
-
-
-
-# TODO: get_user_medias already retrieves a list of all medias for a user
-#       but the information is not complete (or is it? check for failsafe)
-#       use the get_media method as reference, but analize the query for
-#       getting specific media, to see if we can get more information.
-#       if not, then implement the likes for media, and comments for media.
-
-# TODO: Renombrar __get_json_query a get_json_edges
-# TODO: hacer una implementacion especifica para obtener el shorcode media
-#       https://www.instagram.com/graphql/query/?query_hash=477b65a610463740ccdb83135b2014db&variables=%7B%22shortcode%22%3A%22Bu2gZhggNDx%22%2C%22child_comment_count%22%3A3%2C%22fetch_comment_count%22%3A40%2C%22parent_comment_count%22%3A24%2C%22has_threaded_comments%22%3Afalse%7D
-# media = bot.get_media(shortcode="Bu2gZhggNDx")
+# medias = bot.get_user_medias(id=user["id"])
 
 bot.quit()
