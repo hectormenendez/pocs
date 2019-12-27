a06484dbf6f7456314276bf30aab3b2829bc8581
Héctor Menéndez <hector@gik.mx>
2019-12-26 19:14:09 -0600

    js/canvas-snake: the grid system now returns points to be used.

--- src/components/grid.mjs
+++ src/components/grid.mjs
@@ -2,12 +2,12 @@ import { ToFloat, Assignator } from '../core/utils.mjs';
 
 export default class Grid {
     constructor(params) {
-        Assignator({ params, parent: this, required: ['canvas', 'size']});
+        Assignator({ params, parent: this, required: ['canvas', 'size'] });
         // TODO: Should this be calculated every render?
-        this.coordinates = this.getCoordinates();
+        this.coordinates = this.getLines();
     }
 
-    getCoordinates() {
+    getCells() {
         const { element } = this.canvas;
         const x = {
             count: Math.floor(element.width / this.size),
@@ -18,20 +18,31 @@ export default class Grid {
             shift: ToFloat((element.height % this.size) / 2),
         };
 
+        return { x, y };
+    }
+
+    getLines() {
+        const { element } = this.canvas;
+        const { x, y } = this.getCells();
         const shifted = ({ count, shift }) =>
             [...Array(count + 1).keys()].map((i) => ToFloat(this.size * i + shift));
-
         return {
-            x,
-            y,
-            vertical: shifted(x).map((pos) => [
-                [pos, y.shift],
-                [pos, element.height - y.shift],
-            ]),
-            horizontal: shifted(y).map((pos) => [
-                [x.shift, pos],
-                [element.width - x.shift, pos],
-            ]),
+            vertical: shifted(x).map((pos) => ({
+                ini: [pos, y.shift],
+                end: [pos, element.height - y.shift],
+            })),
+            horizontal: shifted(y).map((pos) => ({
+                ini: [x.shift, pos],
+                end: [element.width - x.shift, pos],
+            })),
+        };
+    }
+
+    getPoint({ x, y }) {
+        const { vertical, horizontal } = this.getLines();
+        return {
+            x: vertical[x].ini[0],
+            y: horizontal[y].ini[1],
         };
     }
 
@@ -39,10 +50,10 @@ export default class Grid {
         const { context } = this.canvas;
         const { horizontal, vertical } = this.coordinates;
         [horizontal, vertical].forEach((coordinates) =>
-            coordinates.forEach(([from, to]) => {
+            coordinates.forEach(({ ini, end }) => {
                 context.beginPath();
-                context.moveTo(...from);
-                context.lineTo(...to);
+                context.moveTo(...ini);
+                context.lineTo(...end);
                 context.strokeStyle = this.style;
                 context.stroke();
             }),
--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,22 @@
 import Canvas from './core/canvas.mjs';
 import Grid from './components/grid.mjs';
 
-const canvas = new Canvas(document.getElementsByTagName('canvas')[0]);
+const RESOLUTION = 20;
 
-const grid = new Grid({ canvas, size: 10, style: 'rgba(0,0,0, 0.1)' });
+const canvas = new Canvas(document.getElementsByTagName('canvas')[0]);
+const grid = new Grid({ canvas, size: RESOLUTION, style: 'rgba(0,0,0, 0.1)' });
+
+const { x: { count: xCount }, y: { count: yCount }  } = grid.getCells();
+const point = {
+    x: Math.floor(xCount / 2),
+    y: Math.floor(yCount / 2),
+}
 
 canvas.setRenderer(function Render() {
-    const { context, element } = canvas;
+    const { context } = canvas;
     canvas.clear();
     grid.render();
+    const { x, y } = grid.getPoint(point)
     context.fillStyle = 'red';
-    context.fillRect(element.width / 2, element.height / 2, 10, 10);
+    context.fillRect(x, y, RESOLUTION, RESOLUTION);
 });
