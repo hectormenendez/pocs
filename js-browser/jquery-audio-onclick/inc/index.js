a8f71af58ff3d0bac879fec6fe71e68bdfc711ea
Héctor Menéndez <hector@gik.mx>
2019-12-29 04:09:00 -0600

    js-browser/jquery-audio-onclick: Converted from php to simple html.

--- js-browser/jquery-audio-onclick/inc/index.js
+++ js-browser/jquery-audio-onclick/inc/index.js
@@ -4,19 +4,7 @@ $(document).ready(function(){
 	var $image = $('#image div');
 	var $count = $('#count');
 
-	var fnDown;
-	var toPause;
-
-	// buffer audio
-	/*
-	audio.volume = 0;
-	audio.play();
-    setTimeout(function(){
-	audio.pause();
-	audio.volume = 1;
-    }, 5);
-*/
-	fnDown = function(){
+	var fnDown = function(){
 		$image.unbind('mousedown', fnDown);
 		//audio.stop();
 		audio.play();
@@ -28,20 +16,6 @@ $(document).ready(function(){
 		}, 800);
 
 		$count.html(parseInt($count.html(),10)+1);
-
-		$.post('/index.php', 'count=true', function(data){
-			console.info(data)
-		});
-
-		return;
-
-		$.ajax({
-			url: 'index.php',
-			method: 'post',
-			success: function(data) {
-				console.info(data);
-			}
-		});
 	}
 
 	// mouse presionado
