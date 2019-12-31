c1a16a17a65f5aa65bbd36d54c4eb2d6a2576978
Héctor Menéndez <hector@gik.mx>
2019-12-31 10:45:29 -0600

    js-fullstack/cycle-stage-model-view: back: removed uws dependency, since it didn't work anymore.

--- js-fullstack/cycle-stage-model-view/back/src/index.socket.js
+++ js-fullstack/cycle-stage-model-view/back/src/index.socket.js
@@ -2,5 +2,5 @@ const SocketIO = require('feathers-socketio');
 
 module.exports = SocketIO({
     path     : '/ws/',
-    wsEngine : 'uws', // Use the uWebSocket (uws) module for better performance
+    // wsEngine : 'uws', // Use the uWebSocket (uws) module for better performance
 });
