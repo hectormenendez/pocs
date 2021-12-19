8f317fb59b6aee9389f951025746a451eabc0ca8
Hector Menendez <etor.mx@gmail.com>
2021-12-19 12:32:23 -0600

    setup: eslint fixes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,30 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
     ],
-    plugins: ["svelte3", "@typescript-eslint"],
+    plugins: ["@typescript-eslint", "svelte3"],
     parser: "@typescript-eslint/parser",
     env: {
         es6: true,
         browser: true,
     },
     settings: {
-        "svelte3/typescript": require("typescript"),
+        "svelte3/typescript": () => require("typescript"),
     },
     ignorePatterns: ["node_modules"],
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: "module",
+        extraFileExtensions: [".svelte"],
         tsconfigRootDir: __dirname,
         project: ["./tsconfig.json"],
-        extraFileExtensions: [".svelte"],
     },
     overrides: [
         {
             files: ["*.svelte"],
             processor: "svelte3/svelte3",
         },
+        {
+            files: ["*.ts"],
+        }
     ],
 };
