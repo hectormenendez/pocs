513ae4442bab04fafa7e4e9c43af3952d66255cb
Hector Menendez <etor.mx@gmail.com>
2021-12-19 11:38:50 -0600

    setup: Prettier adjustments.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+    plugins: ["prettier-plugin-svelte"],
     printWidth: 100, // breaks line to maintain code's readability
     useTabs: false, // use spaces for indentation
     tabWidth: 4, // use 4 spaces for indentation
@@ -7,4 +8,8 @@ module.exports = {
     trailingComma: "all", // append trailing comma according to ES5 rules
     bracketSpacing: true, // Enforce spacing on bracketed statements
     arrowParens: "always", // Enforce always using parens on arrow functions
+    svelteSortOrder: "options-scripts-markup-styles",
+    svelteStrictMode: false,
+    svelteBracketNewLine: true,
+    svelteIndentScriptAndStyle: true,
 };
