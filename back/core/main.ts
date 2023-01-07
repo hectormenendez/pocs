60e762ca89e3548fbdf9888bbb03501e39209797
Hector Menendez <hector@gik.mx>
2023-01-07 13:48:47 -0600

    back-core: Main now exposes only one route.

--- back/core/main.ts
+++ back/core/main.ts
@@ -1,9 +1,10 @@
 import { Application, Router, hasFlash, FlashServer } from "x/oak/mod.ts";
 import { Database } from "x/aloedb/mod.ts";
 
-import * as LibResponseHeaderVersion from "./lib/response-header-version.ts";
-import * as LibResponseHeaderTime from "./lib/response-header-time.ts";
-import * as LibLogger from "./lib/logger.ts";
+import * as HandlerCors from "./handler/001-cors.ts";
+import * as HandlerResponseHeaderTime from "./handler/003-response-header-time.ts";
+import * as HandlerResponseHeaderVersion from "./handler/002-response-header-version.ts";
+import * as HandlerLogger from "./handler/004-logger.ts";
 
 type User = {
     nameFirst: string;
@@ -16,28 +17,25 @@ const db = new Database<User>("data/users.json");
 const options = !hasFlash() ? undefined : { serverConstructor: FlashServer };
 const app = new Application(options);
 
-app.use(LibLogger.Handler);
-app.use(LibResponseHeaderTime.Handler);
-app.use(LibResponseHeaderVersion.Handler);
+app.use(HandlerCors.Handler);
+app.use(HandlerLogger.Handler);
+app.use(HandlerResponseHeaderTime.Handler);
+app.use(HandlerResponseHeaderVersion.Handler);
 
 const router = new Router();
 
 router.get("/", async (ctx) => {
-    const users = await db.findMany();
-    ctx.response.body = users;
-});
-
-router.get("/message", (ctx) => {
-    if (!ctx.isUpgradable) {
-        ctx.response.status = 501;
-        return;
-    }
-    const ws = ctx.upgrade();
-    ws.addEventListener("open", () => console.log("open"));
-    ws.addEventListener("close", () => console.log("close"));
-    ws.addEventListener("message", () => console.log("message"));
-    ws.addEventListener("error", () => console.log("error"));
     ctx.response.status = 200;
+    if (!ctx.isUpgradable) {
+        const users = await db.findMany();
+        ctx.response.body = users;
+    } else {
+        const ws = ctx.upgrade();
+        ws.addEventListener("open", () => console.log("open"));
+        ws.addEventListener("close", () => console.log("close"));
+        ws.addEventListener("message", () => console.log("message"));
+        ws.addEventListener("error", () => console.log("error"));
+    }
 });
 
 app.use(router.allowedMethods());
