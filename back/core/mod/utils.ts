09c9289eb4c39ac390daa695b15727ff8c3c8ffb
Hector Menendez <hector@gik.mx>
2023-01-08 12:56:28 -0600

    back-core: Added ImportWalked to utils

--- back/core/mod/utils.ts
+++ back/core/mod/utils.ts
@@ -1,3 +1,8 @@
+import * as $PATH from "std/path/mod.ts";
+import * as $FS from "std/fs/walk.ts";
+
+import { DefaultsCreateService } from "back/core/mod/defaults.ts";
+
 export function Capitalize(str: string): string {
     return [str.charAt(0).toUpperCase(), str.slice(1).toLowerCase()].join("");
 }
@@ -22,5 +27,37 @@ export class Perf {
 
     public elapsed() {
         return (performance.now() - this.start) / Perf.TIME_SECOND;
+    }
+}
+
+export type OptWalk = Required<DefaultsCreateService>;
+
+export type ItemWalk = {
+    $name: string;
+    $uuid: string;
+    $path: string;
+};
+
+export async function ImportWalked<T extends Record<string, unknown>>(
+    path: string,
+    opt: OptWalk,
+): Promise<(ItemWalk & T)[]> {
+    const { optionsWalk, ext } = opt;
+    try {
+        const rx = /^([0-9]+)-(.+)/g;
+
+        const result: (ItemWalk & T)[] = [];
+        for await (const item of $FS.walk(path, optionsWalk)) {
+            const { isFile } = item;
+            if (!isFile) continue;
+            const base = $PATH.basename(item.name, ext);
+            const $uuid = base.replace(rx, "$1");
+            const $name = base.replace(rx, "$2");
+            const rest: T = await import(item.path);
+            result.push({ $name, $uuid, $path: item.path, ...rest });
+        }
+        return result.sort((a, b) => (a.$uuid > b.$uuid ? 1 : -1));
+    } catch (err) {
+        Halt(`Could not Walk over ${path}: ${err.message}`);
     }
 }
