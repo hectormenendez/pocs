c2328178fe7c0b1dd28b93e6221697f773c2e1c2
Hector Menendez <hector@gik.mx>
2023-01-08 10:18:50 -0600

    back-core: Added Perf to utils.

--- back/core/mod/utils.ts
+++ back/core/mod/utils.ts
@@ -10,3 +10,17 @@ export function Halt(...messages: unknown[]): never {
 export function Echo(...messages: unknown[]): void {
     console.log(...messages);
 }
+
+export class Perf {
+    public static readonly TIME_SECOND = 1000;
+
+    private start: number;
+
+    public constructor() {
+        this.start = performance.now();
+    }
+
+    public elapsed() {
+        return (performance.now() - this.start) / Perf.TIME_SECOND;
+    }
+}
